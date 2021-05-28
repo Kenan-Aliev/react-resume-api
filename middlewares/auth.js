94c1b094b6ca3a36c85051cd817b57a6ea2c0450
Kenan2112 <Kenan199948@gmail.com>
2021-05-28 12:13:59 +0600

    fixed auth middleware

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -2,16 +2,20 @@ const jwt = require('jsonwebtoken')
 const config = require('config')
 
 module.exports = function(req,res,next){
-    const token = req.headers.authorization.split(' ')[1]
-    if(!token){
-        return res.status(400).json({message:"Отсутствует токен"})
-    }
-    const decode = jwt.verify(token,config.get("SECRET_KEY"))
-    console.log(decode)
-    if(!decode){
-        return res.status(400).json({message:"Неверный токен"})
+    try {
+        const token = req.headers.authorization.split(' ')[1]
+        if(!token){
+            return res.status(400).json({message:"Отсутствует токен"})
+        }
+        const decode = jwt.verify(token,config.get("SECRET_KEY"))
+        console.log(decode)
+        if(!decode){
+            return res.status(400).json({message:"Неверный токен"})
+        }
+        req.user = decode
+        next()
+    }catch (e) {
+        return res.status(500).json({message:"Возможно,время жизни токена для доступа истекло. Пожалуйста,авторизуйтесь заново"})
     }
 
-    req.user = decode
-    next()
 }
